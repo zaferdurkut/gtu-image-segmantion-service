2ec0d3cda3fa2c298e1270ad3c1ef18458d5c076
zaferdurkut <durkutzafer@gmail.com>
2019-10-19 22:17:52 +0300

    kmeans method locaiton chanced-2

--- kmeans_seg.py
+++ kmeans_seg.py
@@ -30,16 +30,6 @@ gdal_object.open()
 
 #----------------------------------------------------------#
 
-def kmean_clustering(gdal_object, im_dir_path, clusters):
-
-    bands = gdal_object.get_bands()
-    img = gdal_object.im2array()
-    for band_number, band in bands.items():
-        print("\n Band Number: {band_number}".format(band_number=band_number))
-        band_path = create_path(im_dir_path,str(band_number)); create_directory(band_path)
-        img = Utils.im2array_out(band)
-        array = img.reshape((-1, 1))
-        kemans_distance(clusters,array, 'euclidean',band_path)
 
 
 
