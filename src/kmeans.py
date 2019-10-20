2fedc29ebf864aa58fe90f3745eca44682810ba4
zaferdurkut <durkutzafer@gmail.com>
2019-10-20 10:53:44 +0300

    local varience method added

--- src/kmeans.py
+++ src/kmeans.py
@@ -5,6 +5,8 @@ from scipy.spatial.distance import cdist
 import numpy as np
 import matplotlib.pylab as plt
 import json
+from scipy import ndimage
+
 
 @timerfunc
 def kmean_clustering(gdal_object, im_dir_path, clusters, metric):
@@ -28,6 +30,7 @@ def kmean_clustering(gdal_object, im_dir_path, clusters, metric):
         img = Utils.im2array_out(band)
         array = img.reshape((-1, 1))
         kemans_distance(clusters, array, metric, band_path)
+        local_variance(clusters, array, metric, band_path)
 
 
 @timerfunc
@@ -94,6 +97,69 @@ def kemans_distance(clusters, array, metric, out_dir):
 
     return True
 
+@timerfunc
+def local_variance(clusters, array, metric, out_dir):
+    """""
+    
+    Arguments:
+        clusters {list} -- baslangic ve bitis arasinda belli step ile olsuturulmus number_of_clusters sayisi
+        array {array} -- goruntuden elde edilmis array
+        outfile_name {png} -- cikacak grafik pathi
+        metric {text} -- https://docs.scipy.org/doc/scipy-0.15.1/reference/generated/scipy.spatial.distance.cdist.html
+        out_dir {path} -- verilerin cikacagi path
+    Returns:
+        [type] -- [description]
+    """
+    algorithm_path = create_path(out_dir,'Local_Variance'); create_directory(algorithm_path)
+    values_path = create_path(algorithm_path,'values'); create_directory(values_path)
+    graphs_path = create_path(algorithm_path,'graphs'); create_directory(graphs_path)
+
+    meandist_json = {}
+    meandist=[]
+    for number_of_clusters  in clusters:
+        model=KMeans(n_clusters=number_of_clusters) # 
+        model.fit(array)
+        X_clustered = model.labels_
+        # value = ndimage.variance(array, X_clustered)
+        value = ndimage.variance(array)
+        meandist.append(value)
+        meandist_json[number_of_clusters] = value
+
+    diffrences_json = {}
+    diffrences = []
+    for index in range(0,len(meandist)-1):
+        value = meandist[index+1] - meandist[index] 
+        diffrences.append(value)
+        diffrences_json[
+                        str(clusters[index])+
+                        "_"+
+                        str(clusters[index+1])] = value
+    diffrences.append(value)
+
+
+
+    plt.plot(clusters, meandist)
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Average distance')
+    plt.title('Selecting k with the Elbow Method') 
+    plt.savefig(create_path(graphs_path,'euclidean.png'))
+    plt.clf()
+
+    plt.plot(clusters, diffrences)
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Diffrences')
+    plt.title('Selecting k with the Elbow Method') 
+    plt.savefig(create_path(graphs_path,'diffrences.png'))
+    plt.clf()
+
+
+    with open(values_path+'/data.json', 'w') as outfile:
+        json.dump(meandist_json, outfile)
+
+    with open(values_path+'/diffrences.json', 'w') as outfile:
+        json.dump(diffrences_json, outfile)
+
+    return True
 
 @timerfunc
 def kmeans_cluster2raster_example(img, array, outfile_name, driver,n_clusters, gdal_object):
